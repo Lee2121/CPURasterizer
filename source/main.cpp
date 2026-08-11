79749b10e369a30ea68a9406c5bc66f7cf10f553
Lee2121 <lcudney@yahoo.com>
2026-08-11 09:56:13 -0700

    Drawing a color
    Simply drawing a color to the screen

--- source/main.cpp
+++ source/main.cpp
@@ -8,9 +8,10 @@ int main()
 	int height = 600;
 
 	SDL_Window* Window = SDL_CreateWindow("CPU Rasterizer", width, height, SDL_WINDOW_RESIZABLE);
+	SDL_Surface* DrawSurface = nullptr;
 
 	int MouseX = 0;
-	int MoustY = 0;
+	int MouseY = 0;
 
 	bool bRunning = true;
 	while (bRunning)
@@ -19,10 +20,53 @@ int main()
 		{
 			switch (Event.type)
 			{
-				width = Event.window.data1;
-				height = Event.window.data2;
-				break;
+				case SDL_EVENT_WINDOW_RESIZED:
+					if (nullptr != DrawSurface)
+					{
+						SDL_DestroySurface(DrawSurface);
+						DrawSurface = nullptr;
+					}
+					width = Event.window.data1;
+					height = Event.window.data2;
+					break;
+
+				case SDL_EVENT_QUIT:
+					bRunning = false;
+					break;
+
+				case SDL_EVENT_MOUSE_MOTION:
+					MouseX = Event.motion.x;
+					MouseY = Event.motion.y;
+					break;
+
+				case SDL_EVENT_KEY_DOWN:
+					if (SDLK_ESCAPE == Event.key.key)
+					{
+						bRunning = false;
+					}
+					break;
 			}
+		}
+
+		if (bRunning)
+		{
+			DrawSurface = SDL_CreateSurface(width, height, SDL_PIXELFORMAT_ABGR32);
+			SDL_SetSurfaceBlendMode(DrawSurface, SDL_BLENDMODE_NONE);
+
+			SDL_Rect Rect;
+			Rect.x = 0;
+			Rect.y = 0;
+			Rect.w = width;
+			Rect.h = height;
+
+			uint32_t* Pixels = (uint32_t*)DrawSurface->pixels;
+			for (int i = 0; i < width * height; i++)
+			{
+				Pixels[i] = 0xffffdfdf;
+			}
+
+			SDL_BlitSurface(DrawSurface, &Rect, SDL_GetWindowSurface(Window), &Rect);
+			SDL_UpdateWindowSurface(Window);
 		}
 	}
 }
