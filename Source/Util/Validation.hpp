5a334ffeeffe41eb3b99cdc305480a747a12d504
Lee2121 <lcudney@yahoo.com>
2026-08-12 17:38:56 -0700

    Validation.hpp - Added ENSURE_EXIT

--- Source/Util/Validation.hpp
+++ Source/Util/Validation.hpp
@@ -2,9 +2,15 @@
 
 #include <iostream>
 
-#define _ENSURE_IMPL(Expr, EnsureFunc)		if(!!(Expr) || (EnsureFunc, false))
+#define _ENSURE_IMPL(Expr, EnsureFunc)	if(!!(Expr) || (EnsureFunc, false))
+#define _ENSURE_END_IMPL				{} else
 
-#define ENSURE(Expr, ...)					_ENSURE_IMPL(Expr, ValidationInternal::Internal_Ensure(#Expr, __FILE__, __LINE__, ##__VA_ARGS__)) return
+// Triggers an error if the given expression fails, then continues
+#define ENSURE(Expr, ...)				_ENSURE_IMPL(Expr, ValidationInternal::Internal_Ensure(#Expr, __FILE__, __LINE__, ##__VA_ARGS__)) _ENSURE_END_IMPL
+
+// Triggers an error if the given expression fails, and exits the calling function if that occurs.
+#define ENSURE_EXIT(Expr, ...)			ENSURE(Expr, #__VA_ARGS__) return
+
 
 namespace ValidationInternal
 {
